740838b2379d9a7b16e6cbc636d3cb22e4dff37e
Purushottam Kumar <puk@growwthpartners.com>
2025-09-30 15:49:19 +0530

    changed preeti bio description

--- src/components/about/TeamSection.tsx
+++ src/components/about/TeamSection.tsx
@@ -11,7 +11,7 @@ const teamMembers = [{
   name: "Preeti",
   role: "Director, CFO Services",
   image: "/lovable-uploads/f8c7677a-09c4-45c4-bda6-dc581b59d47f.png",
-  description: "A Chartered Accountant with over 20 years of experience in finance and tax. Formerly the Chief Editor at ClearTax, she has a deep understanding of economic and tax policy. She has also worked with PwC and American Express, bringing extensive expertise to Growwth Partners."
+  description: "A Chartered Accountant with over 20 years of experience in finance and tax. Formerly Director at ClearTax. She has also worked with PwC and American Express, bringing extensive expertise to Growwth Partners."
 }, {
   name: "Hemavathy",
   role: "Financial Services Expert",
